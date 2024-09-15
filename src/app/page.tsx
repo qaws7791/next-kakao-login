9332d3815afaf5109f1d3f8414e169e2c1420bfa
qaws7791 <qaws7791@gmail.com>
2024-09-15 17:28:00 +0900

    refactor page component to display user information and add logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
-import KakaoLoginButton from "@/app/kakao-login-button";
-import KakaoLoginLink from "@/app/kakao-login-link";
+import LogoutButton from "@/app/logout-button";
 import { getUser } from "@/server/lib/user";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getUser();
-  if (user) {
-    return redirect("/account");
-  }
+
+  if (!user)
+    return (
+      <main className="flex flex-col items-center justify-between p-24">
+        <h1 className="text-4xl font-bold my-16">
+          Nextjs OAuth with Kakao Login
+        </h1>
+      </main>
+    );
 
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold my-16">
-        Nextjs OAuth with Kakao Login
-      </h1>
-      <KakaoLoginButton />
-      <h2 className="text-xl my-16">or</h2>
-      <KakaoLoginLink />
-    </main>
+    <div className="flex flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold my-16">Welcome, {user.name}</h1>
+      <p className="text-2xl font-medium my-4">KaKao ID: {user.kakaoId}</p>
+
+      <div className="flex flex-col gap-4">
+        {user.photo && (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={user.photo}
+            alt={user.name}
+            width={256}
+            height={256}
+            className="rounded-2xl"
+          />
+        )}
+        <LogoutButton />
+      </div>
+    </div>
   );
 }
