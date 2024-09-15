8e7bdd36d8421edb603ee5385da33e8de384874c
qaws7791 <qaws7791@gmail.com>
2024-09-15 17:25:31 +0900

    Move logout button to /app

--- src/app/logout-button.tsx
+++ src/app/logout-button.tsx
@@ -13,7 +13,7 @@ export default function LogoutButton() {
     <button
       onClick={handleLogout}
       type="button"
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all w-full"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all"
     >
       Logout
     </button>
